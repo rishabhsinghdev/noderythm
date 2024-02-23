a98e197f16fb18b62447b0a64faa4497b589e683
RishabhSinghIO <128444911+rishabhsinghio@users.noreply.github.com>
2024-02-23 17:28:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,8 +55,7 @@ bot.on('message', async (msg) => {
     const durationMinutes = Math.floor(response.data.duration / 60);
     const durationSeconds = (response.data.duration % 60).toFixed(2);
 
-  const replyMessage = `Title: ${response.data.title}\nFilesize: ${fileSizeInMB} MB\nDuration: ${durationMinutes} minutes ${durationSeconds} seconds\nLink: <a href="${response.data.link}">Download</a>`;
-
+      const replyMessage = `Title: ${response.data.title}\nFilesize: ${fileSizeInMB} MB\nDuration: ${durationMinutes} minutes ${durationSeconds} seconds\nDownload: ${response.data.link}`;
 
     bot.sendMessage(chatId, replyMessage);
   } catch (error) {
